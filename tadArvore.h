98c34e9dc4cd82761b4532b488d54dae6c37ce8c
Kauan Eguchi <97849190+kaegg@users.noreply.github.com>
2023-05-03 22:03:35 -0300

    destruir árvore

--- tadArvore.h
+++ tadArvore.h
@@ -1,16 +1,16 @@
 /*
 Alunos:
-Kauan Eiji Kataoka Eguchi 128854
-Henrique Hiroshi Maeda 131594
+Kauan Eiji Kataoka Eguchi
+Henrique Hiroshi Maeda
 */
 
 /*
-Escreva um TADABB para uma ¡rvore Bin·ria de Busca que contÈm as seguintes funÁıes: criar
-·rvore, inserir elementos na ·rvore e destruir ·rvore. A funÁ„o que destrÛi a ·rvore deve liberar
-cada nÛ individualmente. Para isso, utilize um algoritmo para percurso em ·rvore. Esta funÁ„o
-deve receber como par‚metro um ponteiro para um nÛ da ·rvore. A funÁ„o principal (main)
-tambÈm deve ser implementada. Para remover somente nÛs folhas, utilize um dos percursos em
-·rvore bin·ria de busca para chegar nas folhas.
+Escreva um TADABB para uma √Årvore Bin√°ria de Busca que cont√©m as seguintes fun√ß√µes: criar
+√°rvore, inserir elementos na √°rvore e destruir √°rvore. A fun√ß√£o que destr√≥i a √°rvore deve liberar
+cada n√≥ individualmente. Para isso, utilize um algoritmo para percurso em √°rvore. Esta fun√ß√£o
+deve receber como par√¢metro um ponteiro para um n√≥ da √°rvore. A fun√ß√£o principal (main)
+tamb√©m deve ser implementada. Para remover somente n√≥s folhas, utilize um dos percursos em
+√°rvore bin√°ria de busca para chegar nas folhas.
 */
 
 typedef struct no NO;
@@ -26,22 +26,22 @@ struct arvore_binaria {
     NO *raiz;
 };
 
-// Cria uma ·rvore
+// Cria uma √°rvore
 ARVORE_BINARIA *criar_arvore();
 
-// Cria um nÛ
+// Cria um n√≥
 NO *criar_no(int valor);
 
-// inserir valor na ·rvore com um auxiliar
+// inserir valor na √°rvore com um auxiliar
 NO *inserir_aux(NO *no, int valor);
 
-// insere um valor na ·rvore
+// insere um valor na √°rvore
 void inserir(ARVORE_BINARIA *arvore, int valor);
 
-// Percurso em ordem da ·rvore
+// Percurso em ordem da √°rvore
 void posordem_aux(NO *raiz);
 
-// Percurso em ordem da ·rvore com remoÁ„o
+// Percurso em ordem da √°rvore com remo√ß√£o
 void posordem_free(NO *raiz);
 
 void posordem(ARVORE_BINARIA *arvore);
